b49d07280742d3d8d7847bf6c45ea5977323cfac
Katya705 <elizavetamochalova2005@gmail.com>
2025-06-12 20:48:04 +0700

    Update Portfolio.jsx

--- beauty-salon/src/pages/Portfolio/Portfolio.jsx
+++ beauty-salon/src/pages/Portfolio/Portfolio.jsx
@@ -1,4 +1,59 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios'; 
+import styles from './Portfolio.module.scss';
+
 function Portfolio() {
-    return <section><h2>Портфолио</h2><p>Примеры наших работ...</p></section>;
-  }
-  export default Portfolio;
+  // useState — хук для создания управление состояния
+  const [works, setWorks] = useState([]);
+
+  // useEffect — хук для выполнения побочных эффектов в функциональных компонентах
+  useEffect(() => {
+    //запрос на сервер
+    axios.get('http://localhost:3001/portfolio')
+      .then(response => {
+        setWorks(response.data);
+      })
+      .catch(error => {
+        console.error('Ошибка при загрузке данных:', error);
+      });
+  }, []); 
+
+  const manicureImages = works.filter(work => work.imageUrl.toLowerCase().includes('manicure'));
+  const browsImages = works.filter(work => work.imageUrl.toLowerCase().includes('brow'));
+  const studioImages = works.filter(work => work.imageUrl.toLowerCase().includes('studio'));
+
+  return (
+    <div className={styles.portfolio}>
+      <h1 className={styles.title}>Наши работы</h1>
+
+      <section>
+        <h2>Маникюр/Педикюр</h2>
+        <div className={styles.scrollContainer}>
+          {manicureImages.map(work => (
+            <img key={work.id} src={work.imageUrl} alt="" className={styles.image} />
+          ))}
+        </div>
+      </section>
+
+      <section>
+        <h2>Брови</h2>
+        <div className={styles.scrollContainer}>
+          {browsImages.map(work => (
+            <img key={work.id} src={work.imageUrl} alt="" className={styles.image} />
+          ))}
+        </div>
+      </section>
+
+      <section>
+        <h2>Фотографии нашей студии</h2>
+        <div className={styles.scrollContainer}>
+          {studioImages.map(work => (
+            <img key={work.id} src={work.imageUrl} alt="" className={styles.image} />
+          ))}
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Portfolio;
